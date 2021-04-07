c85134ccbf537f4c9103dfb92ea8185f6ab43ec4
MAHADEVAN-A <MAHADEVAN-A@.github.com>
2021-04-07 13:38:42 +0530

    'updates'

--- app.js
+++ app.js
@@ -84,6 +84,6 @@ app.get('/econtact',middlewareFunctions,(req,res)=>{
 
 const port = process.env.PORT || 5000
 
-app.listen(5000,()=>{
+app.listen(port,()=>{
     console.log(`Server listening on port ${port}`);
 })
